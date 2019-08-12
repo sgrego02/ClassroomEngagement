0691d8c026650579014336133576f9a6f6e05188
sg4518 <sg4518@example.com>
2019-08-12 15:12:06 +0300

    Another commit

--- classroom_engagement/urls.py
+++ classroom_engagement/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 from page import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$',views.index,name='index'),
@@ -13,3 +15,5 @@ urlpatterns = [
     url(r'^tools$',views.tools,name='tools'),
     url(r'^history$',views.history,name='history'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
